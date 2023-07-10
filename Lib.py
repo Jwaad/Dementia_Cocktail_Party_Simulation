4d6c175b2e19b4216182b47bb21c11fd1ec6aba2
Jwaad <jwaad@hotmail.co.uk>
2023-07-10 21:07:29 +0100

    Made placeholder scripts

---
+++ Lib.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 18:10:00 2023
+
+@author: Jwaad
+
+Contained within is the class that holds all the useful methods for the creation of the app, to control the dementia simulation.
+Run to "Main.py" To actually run the program
+"""
+
---
+++ Main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 10 18:10:49 2023
+
+@author: Jwaad
+"""
+
