e1c1a745c2a98a2c17e10deaf1cab00c7a7bcbef
Jwaad <jwaad@hotmail.co.uk>
2023-07-11 12:06:04 +0100

    Basic pygame GUI

--- Main.py
+++ Main.py
@@ -5,3 +5,33 @@ Created on Mon Jul 10 18:10:49 2023
 @author: Jwaad
 """
 
+import pygame
+
+
+class DementiaSimulator():
+
+    def __init__(self):
+        self.NumberOfPeople = 0
+        self.Running = True
+
+    def InitScreen(self):
+        self.BackgroundColour = (55, 55, 55)
+        (width, height) = (800, 800)
+        self.Screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Dementia Simulator')
+        self.Screen.fill(self.BackgroundColour)
+
+    def DrawScreen(self):
+        while self.Running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.Running = False
+
+    def Main(self):
+        self.InitScreen()
+        self.DrawScreen()
+
+
+if __name__ == '__main__':
+    dementiaSim = DementiaSimulator()
+    dementiaSim.Main()
